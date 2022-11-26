d4765dc6fcfaf415cc987faf7f30db420c911619
WALL.E <1770957624@qq.com>
2022-11-26 10:23:51 +0800

    third train

--- export.py
+++ export.py
@@ -1,6 +1,7 @@
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
 """
 Export a YOLOv5 PyTorch model to other formats. TensorFlow exports authored by https://github.com/zldrobit
+# python export.py --weights best.pt --include tfjs
 
 Format                      | `export.py --include`         | Model
 ---                         | ---                           | ---
